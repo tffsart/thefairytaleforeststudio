818996f586ec015e98fd2181e7d99976166c628d
tffsart <thefairytaleforeststudio@gmail.com>
2026-06-12 13:08:38 +0000

    Add invitation mode focus behavior

--- script.js
+++ script.js
@@ -189,9 +189,31 @@ const forestPath =
 const forestGate =
     document.querySelector(".forest-gate");   
 
+    const invitation =
+    document.querySelector(
+        ".forest-invitation"
+    );    
+
 if (livingLight) {
 
     let visible = false;
+
+    if (invitation) {
+
+    const rect =
+        invitation.getBoundingClientRect();
+
+    invitationMode =
+
+        rect.top <
+        window.innerHeight * 0.65
+
+        &&
+
+        rect.bottom >
+        window.innerHeight * 0.35;
+
+}
 
     let t = 0;
 
@@ -205,7 +227,7 @@ if (livingLight) {
     let fairyVelocityY = 0;
     
     let returningHome = false;
-   
+    let invitationMode = false;
     function animateLight() {
 
         if (visible) {
@@ -232,6 +254,19 @@ if (returningHome) {
 
     targetWorldY =
         window.innerHeight * 0.35;
+
+} else if (
+    invitationMode &&
+    invitation
+) {
+
+    const rect =
+        invitation.getBoundingClientRect();
+
+    targetWorldY =
+        window.scrollY +
+        rect.top +
+        rect.height * 0.5;
 
 } else {
 
